9a152bdb7f769c8fdba5089de384dbc1bc220ec4
maersta <40675809+maersta@users.noreply.github.com>
2022-07-17 03:08:26 +0200

    Update regex.sh

--- source/regex.sh
+++ source/regex.sh
@@ -1,6 +1,2 @@
-hello=ho02123ware38384you443d34o3434ingtod38384day
-re='(.*)[0-9]+(.*)'
-while [[ $hello =~ $re ]]; do
-  hello=${BASH_REMATCH[1]}${BASH_REMATCH[2]}
-done
-echo "$hello"
+#!/bin/bash
+sed -i 's/foo/linux/g' test.txt
